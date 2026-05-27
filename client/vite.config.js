29dbe3531ddc54cd8e686bf77fe04d4cf9446843
Vijay-912 <vsaini47428@gmail.com>
2026-05-27 11:12:33 +0530

    PMS 404f4

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { copyFileSync } from "fs";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-
+  plugins: [
+    react(),
+    tailwindcss(),
+    {
+      name: "copy-redirects",
+      closeBundle() {
+        copyFileSync("public/_redirects", "dist/_redirects");
+      },
+    },
+  ],
   build: {
     outDir: "dist",
   },
-
   server: {
     host: "0.0.0.0",
     port: 5173,
