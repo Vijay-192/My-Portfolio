62ae39cf6e3a34cac1d53d276086fdc9be867ed9
Vijay-912 <vsaini47428@gmail.com>
2026-05-27 10:57:52 +0530

    PMS 404f2

--- client/src/api/dashboardAPI.js
+++ client/src/api/dashboardAPI.js
@@ -1,5 +1,4 @@
-
-const BASE_URL = import.meta.env.VITE_API_URL;
+const BASE_URL = import.meta.env.VITE_API_BASE_URL; // change var
 
 const get = async (path) => {
   const res = await fetch(`${BASE_URL}${path}`);
